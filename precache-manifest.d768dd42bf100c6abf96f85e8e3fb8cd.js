888a6e28e0972bc3a37a66097ec886f546359d72
Rithik Samanthula <talk2rithiks@gmail.com>
2021-05-05 17:17:28 -0400

    Updates

---
+++ precache-manifest.d768dd42bf100c6abf96f85e8e3fb8cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7c922424ca8fd8ca789e87fd438d40",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "09866767fafa344402c0",
+    "url": "/counter-app/static/js/2.10713e72.chunk.js"
+  },
+  {
+    "revision": "6f5857f47a2b0b4f1734",
+    "url": "/counter-app/static/js/main.6e0dc0c7.chunk.js"
+  },
+  {
+    "revision": "18c5eb179ff4ffdac49c",
+    "url": "/counter-app/static/js/runtime-main.f17966d7.js"
+  }
+]);
